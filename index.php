6afcab65df5facc8ffd06cd6812aaccba2ecb67a
edriso <edriso.dev@gmail.com>
2023-11-25 20:15:36 +0200

    make a router and create controllers directory

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-$heading = "Homepage";
+require 'functions.php';
 
-require "views/index.view.php";
+$uri = $_SERVER['REQUEST_URI'];
+
+if ($uri === '/') {
+    require 'controllers/index.php';
+} elseif ($uri === '/about.php') {
+    require 'controllers/about.php';
+} elseif ($uri === '/contact.php') {
+    require 'controllers/contact.php';
+} 
