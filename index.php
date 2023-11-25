2433377022019933155fef9a709e3e71065d9376
edriso <edriso.dev@gmail.com>
2023-11-25 20:51:49 +0200

    add abort function

--- index.php
+++ index.php
@@ -13,9 +13,5 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
-    http_response_code(404);
-
-    require 'views/404.view.php';
-
-    die();
+    abort();
 }
