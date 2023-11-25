3485c83787387cebc842fe6976ff57421ab6061d
edriso <edriso.dev@gmail.com>
2023-11-25 20:57:19 +0200

    add router file

--- functions.php
+++ functions.php
@@ -11,11 +11,3 @@ function dd($value) {
 function urlIs($value) {
     return $_SERVER['REQUEST_URI'] === $value;
 }
-
-function abort($code = 404) {
-    http_response_code($code);
-
-    require 'views/404.view.php';
-
-    die();
-}
--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
 
 require 'functions.php';
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
-}
+require 'router.php';
