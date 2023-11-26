db8fd500a0104a225ec3bddcb2f7418afe1282c7
edriso <edriso.dev@gmail.com>
2023-11-26 22:22:59 +0200

    insert note, and use htmlspecialchars()

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,9 +1,16 @@
 <?php
 
+$config = require('config.php');
+$db = new Database($config['database']);
+
 $heading = 'Create a note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    dd($_POST);
+$currentUserId = 4;
+$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+    'body' => $_POST['body'],
+    'user_id' => $currentUserId,
+]);
 }
 
 require 'views/note-create.view.php';
--- views/note.view.php
+++ views/note.view.php
@@ -7,7 +7,7 @@
         <p class="mb-4">
             <a href="/notes" class="text-blue-500 hover:underline">go back...</a>
         </p>
-        <p><?= $note['body'] ?></p>
+        <p><?= htmlspecialchars($note['body']) ?></p>
     </div>
 </main>
 
