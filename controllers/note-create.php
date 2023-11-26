7f82fe80019040afa2acc3444d6782fdfd84c466
edriso <edriso.dev@gmail.com>
2023-11-26 23:11:22 +0200

    add a validation layer

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,11 +6,25 @@ $db = new Database($config['database']);
 $heading = 'Create a note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-$currentUserId = 4;
-$db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-    'body' => $_POST['body'],
-    'user_id' => $currentUserId,
-]);
+    $trimmedBody = trim($_POST['body']);
+    $errors = [];
+
+    if(strlen($trimmedBody) === 0) {
+        $errors['body'] = 'Note can not be empty.';
+    }
+
+    if(strlen($trimmedBody) > 1000) {
+        $errors['body'] = 'Note can not be more than 1,000 characters.';
+    }
+
+    if(empty($errors)) {
+        $currentUserId = 4;
+        $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+            'body' => $trimmedBody,
+            'user_id' => $currentUserId,
+        ]);
+        header('Location: /notes');
+    }
 }
 
 require 'views/note-create.view.php';
