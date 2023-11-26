bf3e497f528d72ac619944abe696465afcd62670
edriso <edriso.dev@gmail.com>
2023-11-26 18:36:09 +0200

    add Response file

--- controllers/note.php
+++ controllers/note.php
@@ -4,6 +4,7 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'My Notes';
+$currentUserId = 1; // created a variable to prevent a magic number
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
     'id' => $_GET['id']
@@ -13,9 +14,8 @@ if(!$note) {
     abort();
 }
 
-$currentUserId = 1; // created a variable to prevent a magic number
 if($note['user_id'] !== $currentUserId) {
-    abort(403);
+    abort(Response::FORBIDDEN);
 }
 
 require 'views/note.view.php';
