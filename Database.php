e9822c0227e76efb8d7764dcaf35e49c22a3f105
edriso <edriso.dev@gmail.com>
2023-11-26 20:43:43 +0200

    refactor database api

--- Database.php
+++ Database.php
@@ -3,6 +3,7 @@
 // Connect to the database, and execute a query
 class Database {
     private $connection;
+    private $statement;
 
     public function __construct($config)
     {
@@ -22,10 +23,31 @@ class Database {
 
     public function query($query, $params = [])
     {
-        $statement = $this->connection->prepare($query);
+        $this->statement = $this->connection->prepare($query);
 
-        $statement->execute($params);
+        $this->statement->execute($params);
 
-        return $statement;
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if(!$result) {
+            abort();
+        }
+
+        return $result;
     }
 }
--- controllers/note.php
+++ controllers/note.php
@@ -4,18 +4,12 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'My Notes';
-$currentUserId = 1; // created a variable to prevent a magic number
+$currentUserId = 4; // created a variable to prevent a magic number
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
     'id' => $_GET['id']
-])->fetch();
+])->findOrFail();
 
-if(!$note) {
-    abort();
-}
-
-if($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
 
 require 'views/note.view.php';
