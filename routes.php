a105fd9f525a752b861f9351b5d8cd7e8114e6c1
edriso <edriso.dev@gmail.com>
2023-11-26 21:09:53 +0200

    create routes file

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    '/' => 'controllers/index.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
