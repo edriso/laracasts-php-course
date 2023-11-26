ed7b6ade5fec96be69f2eead919970f56787e028
edriso <edriso.dev@gmail.com>
2023-11-26 23:46:06 +0200

    add Validator class

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -1,26 +1,23 @@
 <?php
 
+require('Validator.php');
+
 $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Create a note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $trimmedBody = trim($_POST['body']);
     $errors = [];
 
-    if(strlen($trimmedBody) === 0) {
-        $errors['body'] = 'Note can not be empty.';
-    }
-
-    if(strlen($trimmedBody) > 1000) {
-        $errors['body'] = 'Note can not be more than 1,000 characters.';
+    if(! Validator::string($_POST['body'], 1, 1000)) {
+        $errors['body'] = 'A body of no more than 1,000 characters is required.';
     }
 
     if(empty($errors)) {
         $currentUserId = 4;
         $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
-            'body' => $trimmedBody,
+            'body' => $_POST['body'],
             'user_id' => $currentUserId,
         ]);
         header('Location: /notes');
