bfafbef49b3338feabb88e3f0faf6dd228655228
edriso <edriso.dev@gmail.com>
2023-11-27 19:39:34 +0200

    refactor notes controller

--- controllers/notes/destroy.php
+++ controllers/notes/destroy.php
@@ -8,12 +8,14 @@ $db = new Database($config['database']);
 $currentUserId = 4;
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', [
-    'id' => $_GET['id']
+    'id' => $_POST['id']
 ])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
 
-view('notes/show', [
-    'heading'=> 'Note',
-    'note'=> $note,
+$db->query('DELETE FROM notes WHERE id = :id', [
+    'id'=> $_POST['id'],
 ]);
+
+header('location: /notes');
+exit();
