d528f026615854678fdfe783bbe00ff088ceb728
edriso <edriso.dev@gmail.com>
2023-11-27 16:44:56 +0200

    use namespacing

--- Core/Database.php
+++ Core/Database.php
@@ -1,20 +1,16 @@
 <?php
 
-// Connect to the database, and execute a query
+namespace Core;
+
+use PDO;
+
 class Database {
     private $connection;
     private $statement;
 
     public function __construct($config)
     {
-        // since $config is dynamic, we pushed it out of the class,
-        // and upward to the next level where we call or instantiate the class
-        // then we created a file that returns config data, as it depends on the env; so we have different file similar like that in production for example
-
-        // http_build_query($config); //"host=localhost&port=3306&dbname=phpapp&charset=utf8mb4"        
-        
         $dsn = 'mysql:' . http_build_query($config, '', ';');
-        // $dsn = "mysql:host={$config['localhost']};port={$config['port']};dbname={$config['dbname']};charset={$config['charset']}";
 
         $this->connection = new PDO($dsn, options: [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
