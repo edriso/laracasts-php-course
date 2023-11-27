90c16b45a6d1055cba127fd7fb2604276a6c4c19
edriso <edriso.dev@gmail.com>
2023-11-27 19:16:52 +0200

    build a better Router

--- views/notes/show.view.php
+++ views/notes/show.view.php
@@ -10,6 +10,7 @@
         <p><?= htmlspecialchars($note['body']) ?></p>
 
         <form method="POST" class="mt-6">
+            <input type="hidden" name="_method" value="DELETE">
             <input type="hidden" name="id" value="<?= $note['id'] ?>">
             <button type="submit" class="text-sm text-red-500">Delete</button>
         </form>
