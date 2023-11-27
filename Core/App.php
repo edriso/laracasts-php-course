1ff0ad17203f2ab7f86a3f60682acb90ec540cd2
edriso <edriso.dev@gmail.com>
2023-11-27 21:37:27 +0200

    make service container

---
+++ Core/App.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Core;
+
+class App {
+    private static $container;
+
+    public static function setContainer($container)
+    {
+        static::$container = $container;
+    }
+
+    public static function container()
+    {
+        return static::$container;
+    }
+    
+    // notice bind and resolve are delegates not duplications
+    public static function bind($key, $resolver)
+    {
+        static::$container->bind($key, $resolver);
+    }
+
+    public static function resolve($key)
+    {
+        return static::$container->resolve($key);
+    }
+}
