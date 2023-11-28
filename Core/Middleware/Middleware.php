fde670f87b857c1c34e1b61629b9356016bddbcb
edriso <edriso.dev@gmail.com>
2023-11-28 19:42:49 +0200

    configure Middleware, and create auth and guest middlewares

---
+++ Core/Middleware/Middleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core\Middleware;
+
+class Middleware
+{
+    public const MAP = [
+        'guest' => Guest::class,
+        'auth' => Auth::class,
+    ];
+
+    public static function resolve($key) {
+        if (!$key) return;
+            // return self::MAP[$key];
+        $middleware = self::MAP[$key] ?? NULL;
+
+        if (! $middleware) {
+            throw new \Exception("No matching middleware found for key '{$key}'.");
+        }
+                
+        (new $middleware)->handle();
+    }
+}
--- Core/router.php
+++ Core/router.php
@@ -2,6 +2,9 @@
 
 namespace Core;
 
+use Core\Middleware\Guest;
+use Core\Middleware\Middleware;
+
 class Router {
     private $routes = [];
 
@@ -9,44 +12,62 @@ class Router {
     // $router->add('GET', '/', 'controller.php');
     public function add($method, $uri, $controller)
     {
-        $this->routes[] = compact('method','uri','controller');
+        // $this->routes[] = compact('method','uri','controller');
+        $this->routes[] = [
+            'method'=> $method,
+            'uri'=> $uri,
+            'controller'=> $controller,
+            'middleware'=>null
+        ];
+        
+        return $this;
     }
 
     public function get($uri, $controller)
     {
-        $this->add('GET', $uri, $controller);
+        return $this->add('GET', $uri, $controller);
     }
 
     public function post($uri, $controller)
     {
-        $this->add('POST', $uri, $controller);
+        return $this->add('POST', $uri, $controller);
     }
 
     public function delete($uri, $controller)
     {
-        $this->add('DELETE', $uri, $controller);
+        return $this->add('DELETE', $uri, $controller);
     }
 
     public function patch($uri, $controller)
     {
-        $this->add('PATCH', $uri, $controller);
+        return $this->add('PATCH', $uri, $controller);
     }
 
     public function put($uri, $controller)
     {
-        $this->add('PUT', $uri, $controller);
+        return $this->add('PUT', $uri, $controller);
     }
 
-    public function route($uri, $method) {
+    public function only($key)
+    {
+        $this->routes[array_key_last($this->routes)]['middleware'] = $key;
+    }
+
+    public function route($uri, $method)
+    {
         foreach ($this->routes as $route) {
             if ($route['uri'] == $uri && $route['method'] === strtoupper($method)) {
+                Middleware::resolve($route['middleware']);
+
                 return require(base_path($route['controller']));
             } 
         }
+
         $this->abort();
     }
 
-    protected function abort($code = 404) {
+    private function abort($code = 404)
+    {
         http_response_code($code);
     
         require(base_path("views/{$code}.php"));
